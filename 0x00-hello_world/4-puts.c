1a4d44cedf39d1df1c8bcca5a4e69db82c95919e
Eager-ToLearn <oarabilebooo@gmail.com>
2023-07-13 18:11:20 -0700

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Print an output without using printf using puts
+ *
+ * Return: 0
+ * Return:0
+ */
+
+int main(void)
+
+{
+
+{
+puts("\"Programming is like building a multilingual puzzle");
+}
+
+return (0);
+}
